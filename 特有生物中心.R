3166429099653760529c8d0e4277d17fd5431f45
r12621209 <163800292+r12621209@users.noreply.github.com>
2024-05-15 23:13:30 +0800

    Update 特有生物中心.R

--- 特有生物中心.R
+++ 特有生物中心.R
@@ -1,8 +1,8 @@
-
-load("C:/Users/User/Downloads/Incidence_data.RData")
+load("C:/Users/lab305_PC1/Downloads/Incidence_data.RData")
 library(glmnet)
 library(compiler)
 library(iNEXT)
+library(dplyr)
 ## 計算物種數的
 r.score = function(x) {
   return( ChaoRichness(x,datatype='incidence_raw')$Estimator)
@@ -86,7 +86,7 @@ opt.train = function(X,n,iter_time) {
       }
       sol[, i] = new_sol
     }
-  
+    
     
     ## mutation(突變，菁英集不變，其他隨機突變，2個1變0，2個0變1，
     #如果突變完比舊的還高就取代)
@@ -96,14 +96,14 @@ opt.train = function(X,n,iter_time) {
       
       if (n.sol == n) {
         repeat {
-        sol.new = sol[, i]
-        pos = c(sample(which(sol[, i] == 0), 2), sample(which(sol[, i] == 1), 2))
-        sol.new[pos] = abs(sol.new[pos] - 1)
-        if (cv.score(X[, cand[sol.new == 1]]) > 0.9) {
-          break
+          sol.new = sol[, i]
+          pos = c(sample(which(sol[, i] == 0), 2), sample(which(sol[, i] == 1), 2))
+          sol.new[pos] = abs(sol.new[pos] - 1)
+          if (cv.score(X[, cand[sol.new == 1]]) > 0.9) {
+            break
+          }
         }
-        }
-        } else if (n.sol > n) {
+      } else if (n.sol > n) {
         repeat {
           sol.new = sol[, i]
           pos = c(sample(which(sol[, i] == 0), 2), sample(which(sol[, i] == 1), (2 + n.sol - n)))
@@ -154,24 +154,24 @@ opt.train = function(X,n,iter_time) {
 }
 opt.train = cmpfun(opt.train)
 
-
 {
-result=list()
-result_nos=list()
-result_cv=list()
-result1=list()
-result_nos1=list()
-result_cv1=list()
-add=matrix(0,26)
-or_cv=matrix(0,26)
-or_nos=matrix(0,26)
+  result=list()
+  result_nos=list()
+  result_cv=list()
+  result1=list()
+  result_nos1=list()
+  result_cv1=list()
+  results=list()
+  add=matrix(0,26)
+  or_cv=matrix(0,26)
+  or_nos=matrix(0,26)
 }
 
-for (i in 1:26) {
-  dat <- incidence_dataset[[i]]$`1 魚類`
+for (i in c(1:13,15:26)) {
+  dat <- incidence_dataset[[i]]$`9 鳥類`
   or_cv[i]=cv.score(dat)
   or_nos[i]=r.score(dat)
-  riv <- substring(colnames(incidence_dataset[[i]][["1 魚類"]]), 1, 6)
+  riv <- substring(colnames(incidence_dataset[[i]][["9 鳥類"]]), 1, 6)
   num_1 <- length(unique(riv))
   out <- estimateD(
     dat,
@@ -190,100 +190,20 @@ for (i in 1:26) {
   best1 <- list()
   nos1 <- list()
   cv1 <- list()
+  answer=list()
   
-  if (out$t > n) {
-    add[i] <- floor(out$t - n)
+  if (out$t >= n) {
+    add[i] <- ceiling(out$t - n)
     out$t <- ncol(dat)
     best <- best1 <-colnames(dat)
-    nos <-best1 <- r.score(dat)
-    cv <-best1 <- cv.score(dat)
-  } else if (choose(n, ceiling(out$t)) > 10^5) {
-    ga <- opt.train(X = dat, n = ceiling(out$t), iter_time = 1000)
-
-    for (k in 1:5) {
-      if (k == 1) { t <- 1 }
-      best1[[k]] <- colnames(dat[, ga[[k]]])
-      nos1[[k]] <- r.score(dat[, ga[[k]]])
-      cv1[[k]] <- cv.score(dat[, ga[[k]]])
-      num_2 <- length(unique(substring(colnames(dat[, ga[[k]]]), 1, 6)))
-      if (num_1 == num_2) {
-        best[[t]] <- colnames(dat[, ga[[k]]])
-        nos[[t]] <- r.score(dat[, ga[[k]]])
-        cv[[t]] <- cv.score(dat[, ga[[k]]])
-        t <- t + 1
-      }
-    }
-  } else {
-    all <- combn(n, ceiling(out$t))
-    r_all <- c(0, 0, 0, 0, 0)
-    for (j in 1:ncol(all)) {
-      if (r.score(dat[, all[, j]]) > min(r_all) & cv.score(dat[, all[, j]]) > 0.9) {
-        loc <- which.min(r_all)
-        r_all[loc] <- r.score(dat[, all[, j]])
-        names(r_all)[loc] <- j
-        r_all <- sort(r_all, decreasing = TRUE)
-      }
-    }
-    r_all <- r_all[r_all != 0]
-    for (k in 1:length(r_all)) {
-      if (k == 1) { t <- 1 }
-      best1[[k]] <- colnames(dat)[all[, as.numeric(names(r_all)[k])]]
-      nos1[[k]] <- r.score(dat[, all[, as.numeric(names(r_all)[k])]])
-      cv1[[k]] <- cv.score(dat[, all[, as.numeric(names(r_all)[k])]])
-      num_2 <- length(unique(substring(colnames(dat)[all[, as.numeric(names(r_all)[k])]], 1, 6)))
-      if (num_1 == num_2) {
-        best[[t]] <- colnames(dat)[all[, as.numeric(names(r_all)[k])]]
-        nos[[t]] <- r.score(dat[, all[, as.numeric(names(r_all)[k])]])
-        cv[[t]] <- cv.score(dat[, all[, as.numeric(names(r_all)[k])]])
-        t <- t + 1
-      }
-    }
-  }
-  result_cv1[[i]] <- cv1
-  result1[[i]] <- best1
-  result_nos1[[i]] <- nos1
-  result_cv[[i]] <- cv
-  result[[i]] <- best
-  result_nos[[i]] <- nos
-}
-for(a in 1:3){
-miss <- c()
-for (i in 1:26) {
-  if ((length(result[[i]]) == 0)) {
-    miss <- c(miss, i)
-  }
-}
-
-for (i in miss) {
-  dat <- incidence_dataset[[i]]$`1 魚類`
-  or_cv[i]=cv.score(dat)
-  or_nos[i]=r.score(dat)
-  riv <- substring(colnames(incidence_dataset[[i]][["1 魚類"]]), 1, 6)
-  num_1 <- length(unique(riv))
-  out <- estimateD(
-    dat,
-    q = 0,
-    datatype = 'incidence_raw',
-    base = "coverage",
-    level = 0.9,
-    nboot = 0,
-    conf = 0.95
-  )
-  
-  n <- ncol(dat)
-  best <- list()
-  nos <- list()
-  cv <- list()
-  best1 <- list()
-  nos1 <- list()
-  cv1 <- list()
-  
-  if (out$t > n) {
-    add[i] <- floor(out$t - n)
-    out$t <- ncol(dat)
-    best <- best1 <-colnames(dat)
-    nos <-best1 <- r.score(dat)
-    cv <-best1 <- cv.score(dat)
+    nos <-nos1 <- r.score(dat)
+    cv <-cv1 <- cv.score(dat)
+    l=data.frame(total=n,choose=out$t,
+                 suggestion = add[i],nos = nos, cv = cv)
+    answer[["site"]]=best
+    answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,best],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+    answer[["information"]]=l
+    results[[i]]=answer
   } else if (choose(n, ceiling(out$t)) > 10^5) {
     ga <- opt.train(X = dat, n = ceiling(out$t), iter_time = 1000)
     
@@ -300,6 +220,16 @@ for (i in miss) {
         t <- t + 1
       }
     }
+    if (t!=1) {
+      l=arrange(data.frame(total=n,choose=ceiling(out$t),
+                           suggestion=ceiling(out$t)-n,nos = unlist(nos), cv = as.numeric(unlist(cv))), desc(nos), desc(cv))[1,]
+      where=best[[which(cv==l$cv & nos==l$nos[1])]]
+      answer[["site"]]=where
+      answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,where],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+      answer[["information"]]=l
+      results[[i]]=answer
+    }
+    
   } else {
     all <- combn(n, ceiling(out$t))
     r_all <- c(0, 0, 0, 0, 0)
@@ -325,7 +255,18 @@ for (i in miss) {
         t <- t + 1
       }
     }
-  }
+    if (t!=1) {
+      l=arrange(data.frame(total=n,choose=ceiling(out$t),
+                           suggestion=ceiling(out$t)-n,nos = unlist(nos), cv = as.numeric(unlist(cv))), desc(nos), desc(cv))[1,]
+      where=best[[which(cv==l$cv & nos==l$nos[1])]]
+      answer[["site"]]=where
+      answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,where],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+      answer[["information"]]=l
+      results[[i]]=answer
+    }
+    
+  } 
+  
   result_cv1[[i]] <- cv1
   result1[[i]] <- best1
   result_nos1[[i]] <- nos1
@@ -333,11 +274,132 @@ for (i in miss) {
   result[[i]] <- best
   result_nos[[i]] <- nos
 }
+
+
+for (rep in 1:3) {
+  miss <- c()
+  for (i in c(1:13,15:26)) {
+    if ((length(result[[i]]) == 0)) {
+      miss <- c(miss, i)
+    }
+  }
+  for (i in miss) {
+    dat <- incidence_dataset[[i]]$`9 鳥類`
+    or_cv[i]=cv.score(dat)
+    or_nos[i]=r.score(dat)
+    riv <- substring(colnames(incidence_dataset[[i]][["9 鳥類"]]), 1, 6)
+    num_1 <- length(unique(riv))
+    out <- estimateD(
+      dat,
+      q = 0,
+      datatype = 'incidence_raw',
+      base = "coverage",
+      level = 0.9,
+      nboot = 0,
+      conf = 0.95
+    )
+    
+    n <- ncol(dat)
+    best <- list()
+    nos <- list()
+    cv <- list()
+    best1 <- list()
+    nos1 <- list()
+    cv1 <- list()
+    answer=list()
+    
+    if (out$t >= n) {
+      add[i] <- ceiling(out$t - n)
+      out$t <- ncol(dat)
+      best <- best1 <-colnames(dat)
+      nos <-nos1 <- r.score(dat)
+      cv <-cv1 <- cv.score(dat)
+      l=data.frame(total=n,choose=out$t,
+                   suggestion = add[i],nos = nos, cv = cv)
+      answer[["site"]]=best
+      answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,best],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+      answer[["information"]]=l
+      results[[i]]=answer
+    } else if (choose(n, ceiling(out$t)) > 10^5) {
+      ga <- opt.train(X = dat, n = ceiling(out$t), iter_time = 1000)
+      
+      for (k in 1:5) {
+        if (k == 1) { t <- 1 }
+        best1[[k]] <- colnames(dat[, ga[[k]]])
+        nos1[[k]] <- r.score(dat[, ga[[k]]])
+        cv1[[k]] <- cv.score(dat[, ga[[k]]])
+        num_2 <- length(unique(substring(colnames(dat[, ga[[k]]]), 1, 6)))
+        if (num_1 == num_2) {
+          best[[t]] <- colnames(dat[, ga[[k]]])
+          nos[[t]] <- r.score(dat[, ga[[k]]])
+          cv[[t]] <- cv.score(dat[, ga[[k]]])
+          t <- t + 1
+        }
+      }
+      if (t!=1) {
+        l=arrange(data.frame(total=n,choose=ceiling(out$t),
+                             suggestion=ceiling(out$t)-n,nos = unlist(nos), cv = as.numeric(unlist(cv))), desc(nos), desc(cv))[1,]
+        where=best[[which(cv==l$cv & nos==l$nos[1])]]
+        answer[["site"]]=where
+        answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,where],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+        answer[["information"]]=l
+        results[[i]]=answer
+      }
+      
+    } else {
+      all <- combn(n, ceiling(out$t))
+      r_all <- c(0, 0, 0, 0, 0)
+      for (j in 1:ncol(all)) {
+        if (r.score(dat[, all[, j]]) > min(r_all) & cv.score(dat[, all[, j]]) > 0.9) {
+          loc <- which.min(r_all)
+          r_all[loc] <- r.score(dat[, all[, j]])
+          names(r_all)[loc] <- j
+          r_all <- sort(r_all, decreasing = TRUE)
+        }
+      }
+      r_all <- r_all[r_all != 0]
+      for (k in 1:length(r_all)) {
+        if (k == 1) { t <- 1 }
+        best1[[k]] <- colnames(dat)[all[, as.numeric(names(r_all)[k])]]
+        nos1[[k]] <- r.score(dat[, all[, as.numeric(names(r_all)[k])]])
+        cv1[[k]] <- cv.score(dat[, all[, as.numeric(names(r_all)[k])]])
+        num_2 <- length(unique(substring(colnames(dat)[all[, as.numeric(names(r_all)[k])]], 1, 6)))
+        if (num_1 == num_2) {
+          best[[t]] <- colnames(dat)[all[, as.numeric(names(r_all)[k])]]
+          nos[[t]] <- r.score(dat[, all[, as.numeric(names(r_all)[k])]])
+          cv[[t]] <- cv.score(dat[, all[, as.numeric(names(r_all)[k])]])
+          t <- t + 1
+        }
+      }
+      if (t!=1) {
+        l=arrange(data.frame(total=n,choose=ceiling(out$t),
+                             suggestion=ceiling(out$t)-n,nos = unlist(nos), cv = as.numeric(unlist(cv))), desc(nos), desc(cv))[1,]
+        where=best[[which(cv==l$cv & nos==l$nos[1])]]
+        answer[["site"]]=where
+        answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,where],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+        answer[["information"]]=l
+        results[[i]]=answer
+      }
+    } 
+    
+    result_cv1[[i]] <- cv1
+    result1[[i]] <- best1
+    result_nos1[[i]] <- nos1
+    result_cv[[i]] <- cv
+    result[[i]] <- best
+    result_nos[[i]] <- nos
+  }
+}
+miss <- c()
+for (i in c(1:13,15:26)) {
+  if ((length(results[[i]]) == 0)) {
+    miss <- c(miss, i)
+  }
 }
 
 for (i in miss) {
-  dat <- incidence_dataset[[i]]$`1 魚類`
-  riv <- substring(colnames(incidence_dataset[[i]][["1 魚類"]]), 1, 6)
+  dat <- incidence_dataset[[i]]$`9 鳥類`
+  riv <- substring(colnames(incidence_dataset[[i]][["9 鳥類"]]), 1, 6)
   num_1 <- length(unique(riv))
   out <- estimateD(
     dat,
@@ -356,26 +418,40 @@ for (i in miss) {
   
   if (choose(n, ceiling(out$t)) > 10^5) {
     ga <- opt.train(X = dat, n = ceiling(out$t), iter_time = 1000)
-    
+    count=c()
     for (k in 1:5) {
-      if (k == 1) { t <- 1 }
-      a <- unique(riv)[which(!unique(riv) %in% unique(substring(colnames(dat[, ga[[k]]]), 1, 6)))]
-      b <- list()
-      for (l in 1:length(a)) {
-        b[[l]] <- grep(a[l], colnames(dat))
-      }
-      num_3 <- expand.grid(b)
-      r_max <- 0
-      for (m in 1:nrow(num_3)) {
-        dat2 <- cbind(dat[, ga[[k]]], dat[, as.numeric(num_3[m, ])])
-        if (r.score(dat2) > r_max) {
-          best[[t]] <- colnames(dat2)
-          cv[[t]] <- cv.score(dat2)
-          nos[[t]] <- r_max <- r.score(dat2)
-        }
-      }
-      t <- t + 1
+      riv2=length(unique(substring(colnames(dat[, ga[[k]]]), 1, 6)))
+      count=c(count,riv2)
     }
+    k=which.max(count)
+    a <- unique(riv)[which(!unique(riv) %in% unique(substring(colnames(dat[, ga[[k]]]), 1, 6)))]
+    if(length(a)==0){
+      dat2=dat[, ga[[k]]]
+      best <- colnames(dat2)
+      cv <- cv.score(dat2)
+      nos <- r_max <- r.score(dat2)
+    }else{b <- list()
+    for (l in 1:length(a)) {
+      b[[l]] <- grep(a[l], colnames(dat))
+    }
+    num_3 <- expand.grid(b)
+    r_max <- 0
+    for (m in 1:nrow(num_3)) {
+      dat2 <- dat[, c(ga[[k]],as.numeric(num_3[m, ]))]
+      if (r.score(dat2) > r_max) {
+        best <- colnames(dat2)
+        cv <- cv.score(dat2)
+        nos <- r_max <- r.score(dat2)
+      }
+    }
+    }
+    l=data.frame(total=n,choose=ncol(dat2),
+                 suggestion=ncol(dat2)-n,nos = nos, cv = cv)
+    
+    answer[["site"]]=best
+    answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,best],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+    answer[["information"]]=l
+    results[[i]]=answer
   } else {
     all <- combn(n, ceiling(out$t))
     r_all <- c(0, 0, 0, 0, 0)
@@ -388,34 +464,42 @@ for (i in miss) {
       }
     }
     r_all <- r_all[r_all != 0]
+    count=c()
     for (k in 1:length(r_all)) {
-      if (k == 1) { t <- 1 }
-      a <- unique(riv)[which(!unique(riv) %in% unique(substring(colnames(dat)[all[, as.numeric(names(r_all)[k])]], 1, 6)))]
-      b <- list()
-      for (l in 1:length(a)) {
-        b[[l]] <- grep(a[l], colnames(dat))
-      }
-      num_3 <- expand.grid(b)
-      r_max <- 0
-      for (m in 1:nrow(num_3)) {
-        dat2 <- cbind(dat[, ga[[k]]], dat[, as.numeric(num_3[m, ])])
-        if (r.score(dat2) > r_max) {
-          best[[t]] <- colnames(dat2)
-          cv[[t]] <- cv.score(dat2)
-          nos[[t]] <- r_max <- r.score(dat2)
-        }
-      }
-      t <- t + 1
+      riv2=length(unique(substring(colnames(dat)[all[, as.numeric(names(r_all)[k])]], 1, 6)))
+      count=c(count,riv2)
     }
+    k=which.max(count)
+    a <- unique(riv)[which(!unique(riv) %in% unique(substring(colnames(dat)[all[, as.numeric(names(r_all)[k])]], 1, 6)))]
+    b <- list()
+    for (l in 1:length(a)) {
+      b[[l]] <- grep(a[l], colnames(dat))
+    }
+    num_3 <- expand.grid(b)
+    r_max <- 0
+    for (m in 1:nrow(num_3)) {
+      dat2 <- dat[,c(all[,as.numeric(names(r_all)[k])],as.numeric(num_3[m, ]))]
+      if (r.score(dat2) > r_max) {
+        best <- colnames(dat2)
+        cv <- cv.score(dat2)
+        nos <- r_max <- r.score(dat2)
+      }
+    }
+    l=data.frame(total=n,choose=ncol(dat2),
+                 suggestion = ncol(dat2)-n,nos = nos, cv = cv)
+    answer[["site"]]=best
+    answer[["ChaoRichness"]]=cbind(ChaoRichness(dat[,best],datatype='incidence_raw'),data.frame(origin_nos=or_nos[i],origin_cv=or_cv[i]))
+    answer[["information"]]=l
+    results[[i]]=answer
   }
   
-  result_cv[[i]] <- cv
-  result[[i]] <- best
-  result_nos[[i]] <- nos
+  result_cv <- cv
+  result <- best
+  result_nos <- nos
+  
 }
 
-save(result,result_cv,add,result_nos,file = 'result_fish')
-
+#results -> summary
 #result ->  推薦河川(覆蓋度 > 0.9，找最大個體數)
 #result1 -> 最後推薦河川 (cov > 0.9，找最大個體數，有每一條支流)
 #or_cv -> 原始全部樣站覆蓋度
@@ -424,7 +508,4 @@ save(result,result_cv,add,result_nos,file = 'result_fish')
 #result_nos1 ->  最後推薦河川覆蓋度
 #result_cv ->  推薦河川個體數
 #result_cv1 ->  最後推薦河川個體數
-
-
-
-
+#add 建議增加
